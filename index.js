47cd3109c5b7d866ffe41c6e828e5c812936ec0e
miguel <miguelevamendo@gmail.com>
2024-09-05 07:51:21 -0600

    ordenamientojs

---
+++ index.js
@@ -0,0 +1,38 @@
+function sumaDeNumeros(num1, num2) {
+  let suma = num1 + num2;
+  return suma;
+}
+console.log(sumaDeNumeros(5, 10));
+
+const estudiante = {
+  nombre: "maria",
+  edad: 25,
+  fechaInicio: "12/09/2024",
+  promedio: 9.5,
+  grado: "universidad",
+};
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const nombres = [
+  "patricio",
+  "bob spanja",
+  "arenita",
+  "calamrdo",
+  "gary",
+  "cangrejo",
+];
+const booleano = [true, false, true, false, true, false, true, false];
+
+const personas = [
+  {
+    nombre: "pedrito",
+    edad: 15,
+  },
+  {
+    nombre: "pancracio",
+    edad:35,
+  }
+];
+
+console.log(personas[1].nombre);
+
---
+++ ordenamiento.js
@@ -0,0 +1,1 @@
+const numerosDesordenados = [3, 4, 25, 2, 45, 10, 1, 56];
