7f1739bb91e6ef138c3be726e7c1ccb5469367e2
miguel <miguelevamendo@gmail.com>
2024-09-10 12:03:48 -0600

    sigue,sigue,sigue y no te detengas

---
+++ dnaStrand.js
@@ -0,0 +1,27 @@
+function dnaStrand(dna) {
+  let texto = dna;
+  let nuevoDna = "";
+  for (let index = 0; index < texto.length; index++) {
+    if (texto[index] == "A") {
+      nuevoDna = nuevoDna + "T"
+    } else if (texto[index] == "T") {
+      nuevoDna = nuevoDna + "A"
+    }else if (texto[index] == "G") {
+      nuevoDna = nuevoDna + "C"
+    }else if (texto[index] == "C") {
+      nuevoDna = nuevoDna + "G"
+    } else {
+      nuevoDna = nuevoDna + texto[index]
+    }
+    
+  }
+  
+  return nuevoDna;
+}
+console.log(dnaStrand());
+
+
+
+//"ATTGC" --> "TAACG"
+//"GTAT" --> "CATA"
+
