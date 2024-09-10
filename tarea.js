510f52a721c88f7ec5211900e3978080bf03ad62
miguel <miguelevamendo@gmail.com>
2024-09-10 11:59:10 -0600

    sigue eres un guerrero

---
+++ tarea.js
@@ -0,0 +1,40 @@
+
+console.log("hola, " + "mundo!");
+
+let nombre = "juan";
+console.log("hola, " + nombre);
+
+let edad = 35;
+console.log("Tengo. " + edad + "años.");
+
+let precio = 20.5;
+console.log("el precio es: $" + precio);
+
+let verdadero = true;
+console.log("El valor es: " + verdadero);
+
+let nulo = null;
+console.log("El valor es: " + nulo);
+
+let indefinido = undefined;
+
+console.log("el valor es:" + indefinido);
+
+let suma = 2 + 5;
+console.log("La suma es: " + suma);
+
+let saludo = "hola"
+let nombres = "pedrito"
+console.log(saludo + " " +  nombre + " " + "mucho gusto");
+
+let numero = 5;
+console.log("El numero es: " + numero.toString());
+
+ //Revisar qué es ecmascript
+
+
+
+
+
+
+
