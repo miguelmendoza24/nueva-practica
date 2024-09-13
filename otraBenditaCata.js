d138cf6554839da16ac79f3c0550591b58c8b990
miguel <miguelevamendo@gmail.com>
2024-09-13 12:07:56 -0600

    estoy cansado jefe

--- otraBenditaCata.js
+++ otraBenditaCata.js
@@ -1,20 +1,3 @@
-/*const binaryArrayToNumber = (arr) => {
-  for (let index = 0; index < arr.length; index++) {
-    
-    
-  }
-  return 0;
-};
-
-
-//El valor de cada posición se obtiene de una potencia de base 2, elevada a un exponente igual a la posición del dígito menos uno.
-//podria hacer que segun la posicion en el indice
-
-
-//relacion entre indice y longitud
-
-//https://meet.google.com/pri-bkso-upn*/
-
 function binaryArrayToNumbe(arr) {
   const ultimoIndice = arr.length - 1;
   let resultado = 0;
@@ -25,4 +8,3 @@ function binaryArrayToNumbe(arr) {
   }
   return resultado;
 }
-
