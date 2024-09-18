49da3e0162bff40709cc329c874c2268130b2a19
miguel <miguelevamendo@gmail.com>
2024-09-18 11:10:30 -0600

    segun papa

---
+++ otrapapa.js
@@ -0,0 +1,10 @@
+function countPositivesSumNegatives(arr) {
+  if (!arr || arr.length === 0) {
+    return []
+  }
+}
+console.log(
+  countPositivesSumNegatives([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15,
+  ])
+);
