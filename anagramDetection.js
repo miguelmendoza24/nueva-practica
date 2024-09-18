01298b342fd383c36c81ae960f54efd9e132d119
miguel <miguelevamendo@gmail.com>
2024-09-18 01:02:06 -0600

    lo logree suiiiiiiiiii

--- anagramDetection.js
+++ anagramDetection.js
@@ -2,21 +2,14 @@ function isAnagram(test, original) {
   if (test.length !== original.length) {
     return false;
   }
-  for (let index = 0; index < test.length; index++) {
-
-  }
-
-};
+  let cadenaOrdenada = test.split("");
+  let cadenaOrdenadaDos = original.split("");
+  cadenaOrdenada.sort();
+  cadenaOrdenadaDos.sort();
+  let cadenaOrdenadaArreglo = cadenaOrdenada.join("");
+  let cadenaOrdenadaDosArreglo = cadenaOrdenadaDos.join("");
+  console.log(cadenaOrdenadaArreglo);
+  console.log(cadenaOrdenadaDosArreglo);
+  return cadenaOrdenadaArreglo === cadenaOrdenadaDosArreglo;
+}
 console.log(isAnagram("foefet", "toffee"));
-
-
-const cadenaConLetrasOrdenadas = (cadena) => {
-  // https://parzibyte.me/blog
-  // Convertir a arreglo
-  const cadenaComoArreglo = cadena.split("");
-  // Ordenar
-  cadenaComoArreglo.sort();
-  // Volver a convertir a cadena
-  const cadenaOrdenada = cadenaComoArreglo.join("");
-  return cadenaOrdenada;
-};
