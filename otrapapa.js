7367d7143ba6a593262cad7baf89e2dcc812d7cd
miguel <miguelevamendo@gmail.com>
2024-09-19 09:28:08 -0600

    vamos

--- otrapapa.js
+++ otrapapa.js
@@ -2,9 +2,19 @@ function countPositivesSumNegatives(arr) {
   if (!arr || arr.length === 0) {
     return []
   }
+  let contarPositivo = 0;
+  let sumarNegativo = 0;
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index] > 0) {
+      contarPositivo ++
+    } else if (arr[index] < 0){ 
+    sumarNegativo += arr[index]
+    }
+  }
+  return [contarPositivo, sumarNegativo]
 }
 console.log(
   countPositivesSumNegatives([
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15,
+    0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14,
   ])
 );
