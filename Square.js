367e5d128776456a4e82a51b91c2b37e67a2765e
miguel <miguelevamendo@gmail.com>
2024-09-19 09:29:05 -0600

    por un futuro mejor

---
+++ Square.js
@@ -0,0 +1,13 @@
+function squareSum(numbers) {
+  let suma = 0;
+  for (let index = 0; index < numbers.length; index++) {
+   // console.log(index);
+    
+    let cuadrado = numbers[index] * numbers[index];
+   // console.log(cuadrado);
+    
+    suma = suma + cuadrado; 
+  }
+  return suma;
+}
+console.log(squareSum([1,2,2]))
