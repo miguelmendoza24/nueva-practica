0ed4f60677c4c71122a14fd27f6825e4baffbd2f
miguel <miguelevamendo@gmail.com>
2024-09-26 12:33:15 -0600

    cada dia mas serca del objetivo

--- yourOrderPlease.js
+++ yourOrderPlease.js
@@ -3,7 +3,6 @@ function identificarPalabras(cadena) {
 
   return array;
 }
-console.log(identificarPalabras("esto es una cadena con varias palabras"));
 
 function buscarPosicion(cadena) {
   for (let index = 0; index < cadena.length; index++) {
@@ -12,7 +11,6 @@ function buscarPosicion(cadena) {
   }
   }
 }
-  console.log(buscarPosicion("est1o"));
   
 function buscarPosiciones(arregloPalabras) {
   let arregloPosiciones = [];
@@ -21,7 +19,6 @@ function buscarPosiciones(arregloPalabras) {
   }
   return arregloPosiciones;
 }
-console.log(buscarPosiciones(["est9a", "es2", "un3a", "caden4a", "con5", "varias6", "palabra7s"]));
 
 function ordenarPalabras(posiciones,palabras) {
   let nuevoArray = [];
@@ -30,27 +27,22 @@ function ordenarPalabras(posiciones,palabras) {
   }
   return nuevoArray;
 }
-console.log(
-  ordenarPalabras(
-    [4, 1, 3, 2, 6, 5],["pal4a", "caden1a", "cald3o", "agu2a", "coca6", "per5ro"]
-  )
-);
-
-["pal4a", "caden1a", "cald3o", "agu2a", "coca6", "per5ro"]
-[4, 1, 3, 2, 6, 5]
 
 function convertirArregloACadena(arregloDeCadena) {
   return arregloDeCadena.join(" ")
 }
-console.log(
-  convertirArregloACadena([
-    "pal4a",
-    "caden1a",
-    "cald3o",
-    "agu2a",
-    "coca6",
-    "per5ro",
-  ])
-);
 
+
+function order(cadena) {
+  if (cadena) {
+    
+    let palabras = identificarPalabras(cadena);
+    let posiciones = buscarPosiciones(palabras);
+    let nuevoArray = ordenarPalabras(posiciones, palabras);
+    let cadenaOrdenada = convertirArregloACadena(nuevoArray)
+    return cadenaOrdenada;
+  }
+  return ""
+}
+//Multiples of 3 or 5
 
